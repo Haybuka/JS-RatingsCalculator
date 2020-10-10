0c1bab2f3d3d5a8daa30ae8d925be8949a953608
Haybuka <paschal.nduleo@gmail.com>
2020-10-10 22:08:38 +0100

    Following tutorial

--- js/ratings.js
+++ js/ratings.js
@@ -7,7 +7,17 @@ function collect_ratings() {
         average: 0
     }
     elements.forEach(element => {
-        let rating = parseInt(element.id.replace('star', ''))
-        ratings.count += parseInt(element.value)
+        rating = parseInt(element.id.replace('star', ''))
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
     });
+    if (ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
+    }
+    return ratings;
 }
+
+document.addEventListener('change', () => {
+    const ratings = collect_ratings();
+    document.querySelector('#average').value = ratings.average.toFixed(2)
+})
